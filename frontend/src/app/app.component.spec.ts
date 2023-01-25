f397f921ec94d8e08c91ee92a4bdb68b98b806ce
Joseph Lu <josephlu414@gmail.com>
2023-01-24 22:51:17 -0500

    Rename files

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sbom'`, () => {
+  it(`should have as title 'wat-map'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sbom');
+    expect(app.title).toEqual('wat-map');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('sbom app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('wat-map app is running!');
   });
 });
